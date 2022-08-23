3483a2d9e34cc13f7330c1207b809635e24e4dff
ThatUche <newuser@Uches-MacBook-Air.local>
2022-08-23 18:08:36 -0400

    added video background

--- newlifekingdom/src/components/Header/Header.jsx
+++ newlifekingdom/src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import VideoBG from '../../assets/videoBG.MP4'
+import Video from '../../assets/VideoBG.mp4'
+import image from '../../assets/NLbanner3.jpg'
 
 const Header = () => {
   return (
     <div className='header'>
-       <video src={VideoBG}/> 
+        <h1>NEW LIFE</h1>
+        <h1>KINGDOM</h1>
+        <h1>MINISTRIES 7th DAY SABBATH</h1>
+       <video loop autoplay>
+        <source className='background'
+          src={Video} type="video/mp4"
+        />
+       </video>
     </div>
   )
 }
